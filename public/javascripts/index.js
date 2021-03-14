d9f6675f01ac7242aa55dc11e7dd37d1ad5edebb
alexdgoncalves <alex.d.goncalves@21@gmail.com>
2021-03-14 17:20:47 -0300

    add setTimeout in the background slide

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,33 +1,43 @@
 window.load = slide(1);
 
-let bgNumber = 1;
-let allBgs = 3;
+var bgNumber = 1;
+var allBgs = 3;
+var clearTime;
 
-function slide(n) {                       
-    document.getElementById('imageBg').style.backgroundImage = `url(img/`+n+`.jpg)`;
-    document.getElementById('imageBg2').style.backgroundImage = `url(img/`+n+`.png)`;
-    document.getElementById('imageBg3').style.backgroundImage = `url(img/`+n+`.svg)`;
+function slide(n) {  
+    document.getElementById('imageBg').style.backgroundImage = `url(img/${n}.jpg)`;                   
+    document.getElementById('imageBg2').style.backgroundImage = `url(img/${n}.png)`;
+    document.getElementById('imageBg3').style.backgroundImage = `url(img/${n}.svg)`;
+    slideComTemporizador()
+}
+
+function slideComTemporizador() {
+    clearTime = setTimeout(() => {
+        proximo();
+        slide(bgNumber);
+    }, 6000); 
 }
 function anterior() {
+    clearTimeout(clearTime);
     if(bgNumber > 1){
         bgNumber--;
         slide(bgNumber);
     } else if(bgNumber == 1){
         bgNumber = 3;
         slide(bgNumber);
-    } else {
-
     }
+    slideComTemporizador();
 }
 
 function proximo() {
+    clearTimeout(clearTime);
+
     if(bgNumber < 3){
         bgNumber++;
         slide(bgNumber);
     } else if(bgNumber == 3){
         bgNumber = 1;
         slide(bgNumber);
-    } else {
-        
     }
+    slideComTemporizador();
 }
