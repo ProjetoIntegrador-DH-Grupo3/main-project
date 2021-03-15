0eabc3f5244b633d136dd55d0b8251eb2c8846e4
Alex Gonçalves <alex.d.goncalves21@gmail.com>
2021-03-14 21:17:33 -0300

    fix slide time out bug

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -4,40 +4,47 @@ var bgNumber = 1;
 var allBgs = 3;
 var clearTime;
 
-function slide(n) {  
-    document.getElementById('imageBg').style.backgroundImage = `url(img/${n}.jpg)`;                   
-    document.getElementById('imageBg2').style.backgroundImage = `url(img/${n}.png)`;
-    document.getElementById('imageBg3').style.backgroundImage = `url(img/${n}.svg)`;
-    slideComTemporizador()
+function slide(n) {
+  document.getElementById(
+    'imageBg',
+  ).style.backgroundImage = `url(img/${n}.jpg)`;
+  document.getElementById(
+    'imageBg2',
+  ).style.backgroundImage = `url(img/${n}.png)`;
+  document.getElementById(
+    'imageBg3',
+  ).style.backgroundImage = `url(img/${n}.svg)`;
+  slideComTemporizador();
 }
 
 function slideComTemporizador() {
-    clearTime = setTimeout(() => {
-        proximo();
-        slide(bgNumber);
-    }, 6000); 
+  clearTimeout(clearTime);
+  clearTime = setTimeout(() => {
+    proximo();
+    slide(bgNumber);
+  }, 6000);
 }
 function anterior() {
-    clearTimeout(clearTime);
-    if(bgNumber > 1){
-        bgNumber--;
-        slide(bgNumber);
-    } else if(bgNumber == 1){
-        bgNumber = 3;
-        slide(bgNumber);
-    }
-    slideComTemporizador();
+  clearTimeout(clearTime);
+  if (bgNumber > 1) {
+    bgNumber--;
+    slide(bgNumber);
+  } else if (bgNumber == 1) {
+    bgNumber = 3;
+    slide(bgNumber);
+  }
+  slideComTemporizador();
 }
 
 function proximo() {
-    clearTimeout(clearTime);
+  clearTimeout(clearTime);
 
-    if(bgNumber < 3){
-        bgNumber++;
-        slide(bgNumber);
-    } else if(bgNumber == 3){
-        bgNumber = 1;
-        slide(bgNumber);
-    }
-    slideComTemporizador();
+  if (bgNumber < 3) {
+    bgNumber++;
+    slide(bgNumber);
+  } else if (bgNumber == 3) {
+    bgNumber = 1;
+    slide(bgNumber);
+  }
+  slideComTemporizador();
 }
