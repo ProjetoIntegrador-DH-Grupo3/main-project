fcf1b294e4f53b5feae613207fb18055e58a4821
Alex Gonçalves <alex.d.goncalves21@gmail.com>
2021-03-30 00:22:12 -0300

    cadastro e autentificacao

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,6 +1,31 @@
+const { Usuario } = require("../sequelize/models");
+const bcrypt = require("bcryptjs");
+
 const usersController = {
-  login: (_req, res) => {
-    res.render("login");
+  index: (_req, res) => res.render("registro"),
+  store: async (req, res) => {
+    const { email, confirm_password, create_password } = req.body;
+
+    if (confirm_password != create_password) {
+      res.redirect("/login");
+    } else {
+      const dataAtual = new Date();
+
+      const usuario = await Usuario.create({
+        permissao: 1,
+        nome: undefined,
+        email: email,
+        senha: bcrypt.hashSync(confirm_password, 8),
+        created_at: dataAtual,
+        update_at: dataAtual,
+      });
+
+      if (!usuario) {
+        return res.send("Houve um erro ao salvar o usuario");
+      }
+
+      res.redirect("/");
+    }
   },
 };
 
