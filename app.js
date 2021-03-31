afa11802cdfdc84fc66afa569ddffd0fc211ee8b
williamwhang <whang.william@gmail.com>
2021-03-31 16:25:55 -0300

    alteração tela de menu e cadastro

--- app.js
+++ app.js
@@ -3,37 +3,26 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const session = require("express-session");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const menuRouter = require("./routes/menu");
-const cadastroRouter = require("./routes/cadastro");
 
 const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.use(
-  session({
-    secret: "dhburguer",
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: false },
-  })
-);
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "/public/")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
+app.use("/login", usersRouter);
 app.use("/menu", menuRouter);
-app.use("/cadastro", cadastroRouter);
-app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
