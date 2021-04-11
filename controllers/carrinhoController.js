611673cd05e5520bf769056d4cdf59bc6a06a0a5
Alex Gonçalves <alex.d.goncalves21@gmail.com>
2021-04-11 14:12:14 -0300

    realizada pagina carrinho

--- controllers/carrinhoController.js
+++ controllers/carrinhoController.js
@@ -1,7 +1,14 @@
+const { Produtos } = require("../sequelize/models");
+
 const express = require("express");
 
 const carrinhoController = {
-  index: (_req, res) => res.render("carrinho"),
+  index: async (_req, res) => {
+    const produtos = await Produtos.findAll({
+      order: [["createdAt", "DESC"]],
+    });
+    res.render("carrinho", { produtos });
+  },
 };
 
 module.exports = carrinhoController;
