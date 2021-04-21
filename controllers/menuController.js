81412ea577a6f0553c5408683ba9268b6ffaf5c9
alexdgoncalves <alex.d.goncalves@21@gmail.com>
2021-04-21 12:19:04 -0300

    itens menu adicionados ao storage

--- controllers/menuController.js
+++ controllers/menuController.js
@@ -1,4 +1,4 @@
-const { Produtos } = require("../sequelize/models");
+const { Produtos, Pedidos } = require("../sequelize/models");
 const menuController = {
   index: async (_req, res) => {
     const produtos = await Produtos.findAll({
@@ -6,6 +6,43 @@ const menuController = {
     });
 
     res.render("menu", { produtos });
+  },
+  store: async (req, res) => {
+    const { produtoId } = req.body;
+    let { produto } = req.session;
+
+    if (!produto) {
+      produto = [];
+    }
+
+    let produtosAtualizados;
+
+    const produtoExistente = produto.find((p) => p.id === produtoId);
+
+    const produtoInfo = await Produtos.findByPk(produtoId);
+
+    if (produtoExistente) {
+      produtosAtualizados = produto.map((p) =>
+        p.id === produtoId ? { ...p, quantidade: p.quantidade + 1 } : p
+      );
+    } else {
+      produtosAtualizados = [
+        ...produto,
+        {
+          id: produtoInfo.id.toString(),
+          nome: produtoInfo.nome,
+          imagem: produtoInfo.imagem,
+          descricao: produtoInfo.descricao,
+          preco: produtoInfo.preco,
+          quantidade: 1,
+        },
+      ];
+    }
+
+    req.session.produto = produtosAtualizados;
+    console.log(req.session.produto);
+
+    return res.redirect("/menu");
   },
 };
 module.exports = menuController;
