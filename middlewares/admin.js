0ebf49077795d6f735259941c4bfb665d8695989
alexdgoncalves <alex.d.goncalves@21@gmail.com>
2021-04-28 23:40:38 -0300

    add lógica middleware admin

--- middlewares/admin.js
+++ middlewares/admin.js
@@ -1,6 +1,7 @@
 module.exports = (req, res, next) => {
-  if (req.session.user.admin == 1) {
-    return  res.render("error404", { url:req.url});
+  console.log(req.session.user);
+  if (req.session.user == undefined || req.session.user.admin == 1) {
+    return res.render("error404", { url: req.url });
   }
 
   return next();
