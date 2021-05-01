5e59365b747937a151bf4b788e5342ed684a9e13
williamwhang <whang.william@gmail.com>
2021-05-01 09:49:27 -0300

    Efeito blur na pagina carrinho na hora de excluir

--- public/javascripts/carrinho.js
+++ public/javascripts/carrinho.js
@@ -54,4 +54,8 @@ for (let index = 0; index < botoes.length; index++) {
   
 }
 
-
+// Efeito blur no carrinho
+function toggle(){
+  var blur = document.getElementById('blur');
+     blur.classList.toggle("active")
+}
