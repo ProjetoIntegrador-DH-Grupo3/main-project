102a56556a86acc44ff028b053c5cb3e799e5c64
williamwhang <whang.william@gmail.com>
2021-05-13 14:44:45 -0300

    ajuste finais

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,7 @@
 const Swal = require("sweetalert2");
 
 module.exports = (req, res, next) => {
+  const { user } = req.session;
   if (user == undefined) {
     res.render("pedidoErro", { user });
   }
